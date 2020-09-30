714c84d767f985eff25fb8a763892270a1f03cb7
Canarypwn <qinfr@shanghaitech.edu.cn>
2020-09-30 14:26:08 +0800

    A1P1 AC solution

--- Assienment 1/P1.cpp
+++ Assienment 1/P1.cpp
@@ -1,40 +1,31 @@
 #include <iostream>
+#include <stack>
 
 using namespace std;
 
-int main(){
-    int num;
-    int array[1000005];
+int main()
+{
+    int num = 0;
+    int ans = 0;
     cin >> num;
-    // cout << num;
-    for (int i=0; i<num; i++){
-        cin >> array[i];
-    }
-    // for (int i=0; i<num; i++){
-    //     cout << array[i] << endl;
-    // }
-    int ans=0;
-    if (num == 0 or num == 1){
-        ans = 0;
-    }
-    else if(num == 2){
-        ans = 1;
-    }
-    else{
-        for (int i=0; i<num-1; i++){   
-            int min = array[i];
-            int max = -1;
-            for (int j=i+1; j<num; j++){
-                if (array[j] >= max and max <= min){
-                    max = array[j];
-                    ans += 1;
-                }
-                // else if (array[i] <= min && max <= min ){
-                //     ans += 1;
-                // }
-            }
-        // cout << ans << endl;
+    stack<int> s;
+    stack<int> s1;
+
+    for(int i=0; i<num; i++){
+        int input;
+        cin >> input;
+        int times=1;
+        while(!s.empty() && s.top() <= input){
+            ans += s1.top();
+            if(s.top() == input) times+=s1.top();
+            s1.pop();
+            s.pop();
         }
+
+        if(!s.empty()) ans++;
+
+        s.push(input);
+        s1.push(times);
     }
     cout << ans;
 }
