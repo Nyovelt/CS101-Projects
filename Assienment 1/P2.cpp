b75ca340f940c6e8ca9e2c131a49401d470e0cbd
Canarypwn <qinfr@shanghaitech.edu.cn>
2020-10-06 12:54:17 +0800

    A1P2

---
+++ Assienment 1/P2.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <algorithm>
+int a[1000006];
+int sub[1000006];
+using namespace std;
+
+int main()
+{
+    int n, k;
+    cin >> n >> k;
+    for (int i = 0; i < n; i++)
+    {
+        cin >> a[i];
+    }
+    sort(a, a + n);
+    for (int i = 0; i < n - 1; i++)
+    {
+        sub[i] = a[i + 1] - a[i];
+    }
+    sort(sub, sub + n - 1);
+    int ans = 0;
+    for (int i = 0; i < k; i++)
+    {
+        ans += sub[i];
+    }
+    cout << ans;
+}
