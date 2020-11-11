1e3df8c150371525f103d96bad80f36e64ad4f98
Canarypwn <qinfr@shanghaitech.edu.cn>
2020-11-11 21:09:59 +0800

    A2P2 AC

--- Assienment 2/P2.cpp
+++ Assienment 2/P2.cpp
@@ -1,43 +1,10 @@
 #include <bits/stdc++.h>
 #define MAXN 1000006
 
-using namespace std;
-
-int n, ans, ans1, siz;
+long long int n, ans, ans1, siz;
 int heap[MAXN];
 int flag[MAXN];
 
-void printHeap()
-{
-    cout << "size: "
-         << siz << " | " << flush;
-    for (int i = 0; i < siz; i++)
-    {
-        cout << heap[i + 1] << " " << flush;
-    }
-    cout << endl;
-}
-
-void min_heapify(int arr[], int start, int end)
-{
-    int fa = start;
-    int chd = 2 * fa + 1;
-    while (chd <= end)
-    {
-        if (arr[chd] > arr[chd + 1] && chd + 1 <= end)
-            chd++;
-        if (arr[fa] < arr[chd])
-        {
-            return;
-        }
-        else
-        {
-            swap(arr[fa], arr[chd]);
-            fa = chd;
-            chd = 2 * fa + 1;
-        }
-    }
-}
 
 void push(int e)
 {
@@ -47,46 +14,66 @@ void push(int e)
         heap[j] = heap[j / 2];
     }
     heap[j] = e;
-    //printHeap();
 }
 
 int pop()
 {
     int ans = heap[1];
     heap[1] = heap[siz--];
-    for (int i = siz / 2 - 1; i >= 0; i--)
-        min_heapify(heap, i, siz - 1);
-    //printHeap();
+
+    // for (int i = siz / 2; i > 0; i--)
+    //     min_heapify( i, siz);
+    
+    int i = 1;
+    while(i*2  <= siz){
+        int j = i * 2;
+        if(heap[j] > heap[j+1] && j+1<=siz){
+            j++;
+        }
+        if (heap[j] > heap[i]) break;
+        std::swap(heap[i], heap[j]);
+        i = j;
+    }
+
+    //min_heapify(siz/2, siz);
     return ans;
 }
 
 int main()
 {
-    cin >> n;
+    scanf("%lld", &n); //n = 100000;
+    srand(time(0));
     for (int i = 0, t; i < n; i++)
     {
-        cin >> t;
-        // cout << "push(t)" << endl;
-        push(t);
-        flag[t]++;
-        if (heap[1] < t)
+        // cin >> t;
+        scanf("%d", &t);
+
+        //t = rand() % 100000 + 100000;
+        if (heap[1] < t && siz)
         {
+           
             // cout << " ans += t - pop();" << endl;
-            cout << t << " " << heap[1] << endl;
-            flag[heap[1]]--;
-            if (flag[heap[1]] < 0)
-            {
+            //std::cout << t << " " << heap[1] << std::endl;
+
+            
+            if(flag[heap[1]] > 0) {
+                flag[heap[1]]--;
             }
-            else
-            {
-                ans1 += 2;
+            else{
+                ans1+=2;
             }
             ans += t - pop();
             // cout << "ans"
             //      << " " << ans << endl;
             // cout << "push(t)" << endl;
-            //push(t);
+            push(t);
+            push(t);
+            flag[t]++;
+        }
+        else{
+            push(t);
         }
     }
-    cout << ans << " " << ans1 << flush;
+    //cout << ans << " " << ans1 << endl;
+    printf("%lld %lld\n", ans, ans1);
 }
---
+++ Assienment 2/a.py
@@ -0,0 +1,6 @@
+import random
+
+a = 1000000
+print(a)
+for i in range(0,a):
+    print(random.randint(0,999999),end = " ")
